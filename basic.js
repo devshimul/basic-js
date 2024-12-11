44833b40c4eaddd5d4a8614cf90c6e7af36c1220
devshimul <shimul.bisd@gmail.com>
2024-12-11 16:19:03 +0600

    third commit

--- basic.js
+++ basic.js
@@ -1,8 +1,12 @@
+/* 
 var age = 16;
 // console.log(age);
 
 var tomattoPrice = 150;
 // console.log(tomattoPrice);
+
+
+
 
 var fullName = "Shimul";
 // console.log(fullName);
@@ -26,3 +30,40 @@ console.log(a);
 
 const applePrice = 350;
 console.log(applePrice);
+*/
+// Data type
+// Number
+// String
+// Boolean
+// Undefined
+//NaN Not a number
+
+let x = 10;
+// console.log(typeof x);
+let myName = "Shimul Hossain";
+// console.log(typeof myName);
+let hasDrivesLicense = true;
+let isHotWeather = false;
+let eatChickenBiriyani = true;
+
+// console.log(typeof hasDrivesLicense);
+let age = 27;
+// console.log(typeof age);
+
+let a = 15;
+let result = a + myName;
+// console.log(result);
+
+//String concatination and coartion
+let y = "8";
+let total = x - y;
+console.log(total);
+
+let newValue = "15";
+// console.log(newValue);
+
+let firstName = "Shimul ";
+let lastName = "Hossain";
+let fullName = firstName + lastName;
+// console.log(firstName.concat(lastName));
+// console.log(fullName);
