b101432bd44e843a248a30d69c8c6b23134425a8
devshimul <shimul.bisd@gmail.com>
2024-12-11 15:52:55 +0600

    js variable

--- basic.js
+++ basic.js
@@ -1,2 +1,28 @@
-console.log("Tomatto");
-console.log("shimul");
+var age = 16;
+// console.log(age);
+
+var tomattoPrice = 150;
+// console.log(tomattoPrice);
+
+var fullName = "Shimul";
+// console.log(fullName);
+var x = 25;
+var y = 25;
+var result = x + y;
+// console.log(result);
+
+var number;
+number = 10;
+// console.log(number);
+
+var potatoPrice = 80;
+console.log(typeof y);
+console.log(x === y);
+
+let a = 45;
+a = 55;
+a = 100;
+console.log(a);
+
+const applePrice = 350;
+console.log(applePrice);
